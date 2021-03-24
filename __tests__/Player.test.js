36c6440132299ffae8e366daa6940fd723d8dc03
Ryobia <ryobia36@gmail.com>
2021-03-24 17:39:43 -0600

    isAlive, reduceHealth, and get Health

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -13,7 +13,7 @@ test('creates a player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
       );
-  });
+});
 
   test('gets player stats as an object', () => {
       const player = new Player('Steve');
@@ -32,4 +32,33 @@ test('creates a player object', () => {
       player.inventory = [];
 
       expect(player.getInventory()).toEqual(false);
+  });
+
+test("gets player's health value", () => {
+  const player = new Player('Dave');
+  
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+ });
+
+ test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+  
+    expect(player.isAlive()).toBeTruthy();
+  
+    player.health = 0;
+  
+    expect(player.isAlive()).toBeFalsy();
+  });
+
+  test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+  
+    player.reduceHealth(5);
+  
+    expect(player.health).toBe(oldHealth - 5);
+  
+    player.reduceHealth(99999);
+  
+    expect(player.health).toBe(0);
   });
